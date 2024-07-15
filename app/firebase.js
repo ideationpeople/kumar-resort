3b2c53dade924897ac86dcba295c022008723f16
Bimal2002 <bimalgayali@gmail.com>
2024-07-15 11:47:41 +0530

    Fix Firebase initialization

--- app/firebase.js
+++ app/firebase.js
@@ -1,34 +1,34 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { EmailAuthProvider } from "firebase/auth";
-import { getAuth } from "firebase/auth";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore"
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+// import { EmailAuthProvider } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBxHqaq23UsnrqPog5KUVCVW67utlW9Uic",
-  authDomain: "unwindhotel-19f42.firebaseapp.com",
-  projectId: "unwindhotel-19f42",
-  storageBucket: "unwindhotel-19f42.appspot.com",
-  messagingSenderId: "789522862509",
-  appId: "1:789522862509:web:de7305b910b62f88cb5124",
-  measurementId: "G-YKE2WCDQTS"
-};
-// Initialize Firebase
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBxHqaq23UsnrqPog5KUVCVW67utlW9Uic",
+//   authDomain: "unwindhotel-19f42.firebaseapp.com",
+//   projectId: "unwindhotel-19f42",
+//   storageBucket: "unwindhotel-19f42.appspot.com",
+//   messagingSenderId: "789522862509",
+//   appId: "1:789522862509:web:de7305b910b62f88cb5124",
+//   measurementId: "G-YKE2WCDQTS"
+// };
+// // Initialize Firebase
 
-let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-const provider = new EmailAuthProvider();
+// let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+// const provider = new EmailAuthProvider();
+// // const db = getFirestore(app);
+// const auth = getAuth(app);
+
+// export { provider, auth };
+// // export default db;
+// // const app = initializeApp(firebaseConfig);
 // const db = getFirestore(app);
-const auth = getAuth(app);
 
-export { provider, auth };
-// export default db;
-// const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export {db};
+// export {db};
 
 
 
@@ -47,3 +47,30 @@ export {db};
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { EmailAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxHqaq23UsnrqPog5KUVCVW67utlW9Uic",
+  authDomain: "unwindhotel-19f42.firebaseapp.com",
+  projectId: "unwindhotel-19f42",
+  storageBucket: "unwindhotel-19f42.appspot.com",
+  messagingSenderId: "789522862509",
+  appId: "1:789522862509:web:de7305b910b62f88cb5124",
+  measurementId: "G-YKE2WCDQTS"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new EmailAuthProvider();
+
+export { db, auth, provider };
